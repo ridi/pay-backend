eab69b3adffa5fdce476dc1b5f37a8c5938cd013
Gyu Kang <me@amolorsy.me>
2018-07-26 15:12:49 +0900

    Remove DoctrineMigrationBundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true]
 ];
