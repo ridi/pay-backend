7f743fa77b1d7824a3ef84c0dec8d50b7518f0f0
Gyu Kang <me@amolorsy.me>
2018-07-27 10:48:09 +0900

    Use SimpleAnnotationReader representations

--- src/Entity/CreditCardIssuerEntity.php
+++ src/Entity/CreditCardIssuerEntity.php
@@ -2,34 +2,32 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
 /**
- * @ORM\Table(name="credit_card_issuer", indexes={@ORM\Index(name="idx_pg_id", columns={"pg_id"})})
- * @ORM\Entity
+ * @Table(name="credit_card_issuer", indexes={@Index(name="idx_pg_id", columns={"pg_id"})})
+ * @Entity
  */
 class CreditCardIssuerEntity
 {
     /**
      * @var int
      *
-     * @ORM\Column(name="id", type="integer", nullable=false, options={"unsigned"=true})
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @Column(name="id", type="integer", nullable=false, options={"unsigned"=true})
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="code", type="string", length=16, nullable=false, options={"comment"="카드 발급사 코드"})
+     * @Column(name="code", type="string", length=16, nullable=false, options={"comment"="카드 발급사 코드"})
      */
     private $code = '';
 
     /**
      * @var string
      *
-     * @ORM\Column(name="name", type="string", length=16, nullable=false, options={"comment"="카드 발급사 이름"})
+     * @Column(name="name", type="string", length=16, nullable=false, options={"comment"="카드 발급사 이름"})
      */
     private $name = '';
 }
--- src/Entity/PayMethodEntity.php
+++ src/Entity/PayMethodEntity.php
@@ -2,20 +2,18 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
 /**
- * @ORM\Table(name="pay_method", uniqueConstraints={@ORM\UniqueConstraint(name="uniq_uuid", columns={"uuid"})}, indexes={@ORM\Index(name="idx_u_idx", columns={"u_idx"})})
- * @ORM\Entity
+ * @Table(name="pay_method", uniqueConstraints={@UniqueConstraint(name="uniq_uuid", columns={"uuid"})}, indexes={@Index(name="idx_u_idx", columns={"u_idx"})})
+ * @Entity
  */
 class PayMethodEntity
 {
     /**
      * @var int
      *
-     * @ORM\Column(name="id", type="integer", nullable=false, options={"unsigned"=true})
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @Column(name="id", type="integer", nullable=false, options={"unsigned"=true})
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
@@ -23,28 +21,28 @@ class PayMethodEntity
      * TODO: https://github.com/ramsey/uuid-doctrine 도입
      * @var binary
      *
-     * @ORM\Column(name="uuid", type="binary", nullable=false, options={"comment"="id 값 유추 방지를 위한 uuid"})
+     * @Column(name="uuid", type="binary", nullable=false, options={"comment"="id 값 유추 방지를 위한 uuid"})
      */
     private $uuid;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="type", type="string", length=0, nullable=false, columnDefinition="ENUM('CARD')", options={"default"="CARD","comment"="결제 수단"})
+     * @Column(name="type", type="string", length=0, nullable=false, columnDefinition="ENUM('CARD')", options={"default"="CARD","comment"="결제 수단"})
      */
     private $type = 'CARD';
 
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="created_at", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP","comment"="결제 수단 등록 시각"})
+     * @Column(name="created_at", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP","comment"="결제 수단 등록 시각"})
      */
     private $created_at = 'CURRENT_TIMESTAMP';
 
     /**
      * @var \DateTime|null
      *
-     * @ORM\Column(name="deleted_at", type="datetime", nullable=true, options={"comment"="결제 수단 삭제 시각"})
+     * @Column(name="deleted_at", type="datetime", nullable=true, options={"comment"="결제 수단 삭제 시각"})
      */
     private $deleted_at;
 }
