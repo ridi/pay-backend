95a84751c96ef9fafe5845b6fbc89919da2abece
Gyu Kang <me@amolorsy.me>
2018-08-13 14:18:12 +0900

    REMOVE CARD -> DELETE CARD

--- src/User/Constant/UserActionConstant.php
+++ src/User/Constant/UserActionConstant.php
@@ -6,7 +6,7 @@ namespace RidiPay\User\Constant;
 abstract class UserActionConstant
 {
     const ADD_CARD = 'ADD_CARD';
-    const REMOVE_CARD = 'REMOVE_CARD';
+    const DELETE_CARD = 'DELETE_CARD';
     const UPDATE_PIN = 'UPDATE_PIN';
     const ENABLE_ONETOUCH_PAY = 'ENABLE_ONETOUCH_PAY';
     const DISABLE_ONETOUCH_PAY = 'DISABLE_ONETOUCH_PAY';
