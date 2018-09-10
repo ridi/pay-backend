361c1efd6104292ff281fc368bd96d3b1d4241f6
Gyu Kang <me@amolorsy.me>
2018-09-10 17:29:54 +0900

    Remove usages of TimeConstant in 'ridibooks/core'

--- src/User/Domain/Service/PinEntryAbuseBlockPolicy.php
+++ src/User/Domain/Service/PinEntryAbuseBlockPolicy.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace RidiPay\User\Domain\Service;
 
-use Ridibooks\Library\TimeConstant;
+use RidiPay\Library\TimeUnitConstant;
 
 class PinEntryAbuseBlockPolicy implements BaseAbuseBlockPolicy
 {
@@ -20,7 +20,7 @@ class PinEntryAbuseBlockPolicy implements BaseAbuseBlockPolicy
      */
     public function getBlockedPeriod(): int
     {
-        return 10 * TimeConstant::SEC_IN_MINUTE;
+        return 10 * TimeUnitConstant::SEC_IN_MINUTE;
     }
 
     /**
