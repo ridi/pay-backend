065f01a32ba68b5c821f408fa875d01fec2e7c29
Gyu Kang <me@amolorsy.me>
2018-09-20 19:46:00 +0900

    Cover error response assertions in UserInformationInquiryTest

--- tests/src/Controller/UserInformationInquiryTest.php
+++ tests/src/Controller/UserInformationInquiryTest.php
@@ -3,58 +3,97 @@ declare(strict_types=1);
 
 namespace RidiPay\Tests\Controller;
 
+use Ridibooks\OAuth2\Authorization\Exception\AuthorizationException;
+use RidiPay\Controller\Response\UserErrorCodeConstant;
 use RidiPay\Tests\TestUtil;
 use RidiPay\User\Application\Service\UserAppService;
-use Symfony\Bundle\FrameworkBundle\Client;
+use RidiPay\User\Domain\Exception\LeavedUserException;
+use RidiPay\User\Domain\Exception\NotFoundUserException;
+use RidiPay\User\Domain\Exception\OnetouchPaySettingChangeDeclinedException;
+use RidiPay\User\Domain\Exception\WrongFormattedPinException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserInformationInquiryTest extends ControllerTestCase
 {
-    /** @var Client */
-    private static $client;
-
-    /** @var string */
-    private static $payment_method_id;
-
-    public static function setUpBeforeClass()
-    {
-        $u_idx = TestUtil::getRandomUidx();
-        UserAppService::createUser($u_idx);
-        UserAppService::updatePin($u_idx, '123456');
-        UserAppService::enableOnetouchPay($u_idx);
-
-        self::$payment_method_id = TestUtil::createCard($u_idx);
-
-        self::$client = self::createClientWithOAuth2AccessToken();
+    /**
+     * @dataProvider userProvider
+     *
+     * @param int $u_idx
+     * @param null|string $payment_method_id
+     * @param int $http_status_code
+     * @param null|string $error_code
+     * @throws AuthorizationException
+     */
+    public function testUserInformationInquiry(
+        int $u_idx,
+        ?string $payment_method_id,
+        int $http_status_code,
+        ?string $error_code
+    ) {
         TestUtil::setUpOAuth2Doubles($u_idx, TestUtil::U_ID);
-    }
 
-    public static function tearDownAfterClass()
-    {
+        $client = self::createClientWithOAuth2AccessToken();
+        $client->request(Request::METHOD_GET, '/me');
+
+        $response_status_code = $client->getResponse()->getStatusCode();
+        $response_content = $client->getResponse()->getContent();
+
+        $this->assertSame($http_status_code, $response_status_code);
+        if ($response_status_code === Response::HTTP_OK) {
+            $expected_response = json_encode([
+                'payment_methods' => [
+                    'cards' => [
+                        [
+                            'iin' => substr(TestUtil::CARD['CARD_NUMBER'], 0, 6),
+                            'issuer_name' => '신한카드',
+                            'color' => '000000',
+                            'logo_image_url' => '',
+                            'subscriptions' => [],
+                            'payment_method_id' => $payment_method_id
+                        ]
+                    ]
+                ],
+                'has_pin' => true,
+                'is_using_onetouch_pay' => true
+            ]);
+            $this->assertSame($expected_response, $response_content);
+        }
+
+        $decoded_response_content = json_decode($response_content);
+        if (isset($decoded_response_content->code)) {
+            $this->assertSame($error_code, $decoded_response_content->code);
+        }
+
         TestUtil::tearDownOAuth2Doubles();
     }
 
-    public function testUserInformationInquiry()
+    /**
+     * @return array
+     * @throws LeavedUserException
+     * @throws NotFoundUserException
+     * @throws OnetouchPaySettingChangeDeclinedException
+     * @throws WrongFormattedPinException
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Throwable
+     */
+    public function userProvider(): array
     {
-        self::$client->request(Request::METHOD_GET, '/me');
-        $expected_response = json_encode([
-            'payment_methods' => [
-                'cards' => [
-                    [
-                        'iin' => substr(TestUtil::CARD['CARD_NUMBER'], 0, 6),
-                        'issuer_name' => '신한카드',
-                        'color' => '000000',
-                        'logo_image_url' => '',
-                        'subscriptions' => [],
-                        'payment_method_id' => self::$payment_method_id
-                    ]
-                ]
-            ],
-            'has_pin' => true,
-            'is_using_onetouch_pay' => true
-        ]);
-        $this->assertSame(Response::HTTP_OK, self::$client->getResponse()->getStatusCode());
-        $this->assertSame($expected_response, self::$client->getResponse()->getContent());
+        $user_indices = [TestUtil::getRandomUidx(), TestUtil::getRandomUidx(), TestUtil::getRandomUidx()];
+
+        UserAppService::createUser($user_indices[0]);
+        UserAppService::updatePin($user_indices[0], '123456');
+        UserAppService::enableOnetouchPay($user_indices[0]);
+        $payment_method_id = TestUtil::createCard($user_indices[0]);
+
+        UserAppService::createUser($user_indices[1]);
+        UserAppService::deleteUser($user_indices[1]);
+
+        return [
+            [$user_indices[0], $payment_method_id, Response::HTTP_OK, null],
+            [$user_indices[1], null, Response::HTTP_FORBIDDEN, UserErrorCodeConstant::LEAVED_USER],
+            [$user_indices[2], null, Response::HTTP_NOT_FOUND, UserErrorCodeConstant::NOT_FOUND_USER]
+        ];
     }
 }
