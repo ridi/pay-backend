7eb0a2683242d9b7fb37133831b0426bbea32848
Gyu Kang <me@amolorsy.me>
2018-10-31 22:59:24 +0900

    Change a message for invalid card information

--- src/Pg/Domain/Exception/CardRegistrationException.php
+++ src/Pg/Domain/Exception/CardRegistrationException.php
@@ -14,7 +14,7 @@ class CardRegistrationException extends PgException
     {
         $message = '카드 등록에 실패했습니다.';
         if ($response->isUnmatchedCardInformation()) {
-            $message .= "\n3회 실패 시 당일 거래가 제한됩니다.";
+            $message .= "\n3회 이상 실패 시 당일 거래가 제한될 수 있습니다.";
         }
 
         parent::__construct($message, $response->getResponseMessage());
