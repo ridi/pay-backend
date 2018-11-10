21294bd9d8692a06a3576d0ea4ed1e59c1b51002
Gyu Kang <me@amolorsy.me>
2018-11-10 13:32:55 +0900

    Check mariadb and redis availabilty in health check

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace RidiPay\Controller;
 
+use Predis\Client;
+use RidiPay\Library\ConnectionProvider;
+use RidiPay\Library\SentryHelper;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,21 +15,37 @@ class HomeController extends BaseController
     /**
      * @Route("/health-check", methods={"GET"})
      *
-     * @return Response
+     * @return JsonResponse
      */
-    public function healthCheck(): Response
+    public function healthCheck(): JsonResponse
     {
-        return new Response("It's healthy");
+        try {
+            $db = ConnectionProvider::getConnection();
+            if (!$db->ping()) {
+                throw new \Exception('MariaDB connection is not working.');
+            }
+
+            $redis = new Client(['host' => getenv('REDIS_HOST')]);
+            if (!$redis->ping()) {
+                throw new \Exception('Redis connection is not working.');
+            }
+        } catch (\Throwable $t) {
+            SentryHelper::captureMessage($t->getMessage(), [], [], true);
+
+            return new JsonResponse(null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return new JsonResponse();
     }
 
     /**
      * Ignore requests for favicon.ico
      * @Route("/favicon.ico", methods={"GET"})
      *
-     * @return Response
+     * @return JsonResponse
      */
-    public function favicon(): Response
+    public function favicon(): JsonResponse
     {
-        return new Response('', Response::HTTP_NO_CONTENT);
+        return new JsonResponse();
     }
 }
