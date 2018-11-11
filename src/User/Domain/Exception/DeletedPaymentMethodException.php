4547a1a027fb847432b2c3ac28d2eee1fd091501
Gyu Kang <me@amolorsy.me>
2018-11-11 23:18:24 +0900

    Fix typos

--- src/User/Domain/Exception/DeletedPaymentMethodException.php
+++ src/User/Domain/Exception/DeletedPaymentMethodException.php
@@ -8,7 +8,7 @@ class DeletedPaymentMethodException extends \Exception
     /**
      * @param string $message
      */
-    public function __construct(string $message = '등록되지 않은 결제 수단입니다.')
+    public function __construct(string $message = '삭제된 결제 수단입니다.')
     {
         parent::__construct($message);
     }
