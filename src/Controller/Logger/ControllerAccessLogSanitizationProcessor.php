98fe84c3ac557a368b970176e69171f260c50180
Gyu Kang <me@amolorsy.me>
2018-12-07 10:17:21 +0900

    Match only key names when sanitizing request body

--- src/Controller/Logger/ControllerAccessLogSanitizationProcessor.php
+++ src/Controller/Logger/ControllerAccessLogSanitizationProcessor.php
@@ -34,19 +34,19 @@ class ControllerAccessLogSanitizationProcessor implements ProcessorInterface
      */
     private static function sanitize(string $key, $value)
     {
-        if (self::isCardNumber($key, $value)) {
+        if (self::isCardNumber($key)) {
             $value = substr($value, 0, 6) . str_repeat('*', strlen($value) - 6);
-        } elseif (self::isCardExpirationDate($key, $value)) {
+        } elseif (self::isCardExpirationDate($key)) {
             $value = '****';
-        } elseif (self::isCardPassword($key, $value)) {
+        } elseif (self::isCardPassword($key)) {
             $value = '**';
-        } elseif (self::isBirthDate($key, $value)) {
+        } elseif (self::isTaxId($key)) {
+            $value = str_repeat('*', strlen($value));
+        } elseif (self::isPin($key)) {
             $value = '******';
-        } elseif (self::isPin($key, $value)) {
-            $value = '******';
-        } elseif (self::isBuyerName($key, $value)) {
+        } elseif (self::isBuyerName($key)) {
             $value = str_repeat('*', mb_strlen($value));
-        } elseif (self::isBuyerEmail($key, $value)) {
+        } elseif (self::isBuyerEmail($key)) {
             [$id, $domain] = explode('@', $value);
             $value = str_repeat('*', mb_strlen($id)) . '@' . str_repeat('*', mb_strlen($domain));
         }
@@ -56,71 +56,64 @@ class ControllerAccessLogSanitizationProcessor implements ProcessorInterface
 
     /**
      * @param string $key
-     * @param $value
      * @return bool
      */
-    private static function isCardNumber(string $key, $value): bool
+    private static function isCardNumber(string $key): bool
     {
-        return $key === 'card_number' && is_string($value) && preg_match('/^\d{13,16}$/', $value);
+        return $key === 'card_number';
     }
 
     /**
      * @param string $key
-     * @param $value
      * @return bool
      */
-    private static function isCardExpirationDate($key, $value): bool
+    private static function isCardExpirationDate($key): bool
     {
-        return $key === 'card_expiration_date' && is_string($value) && preg_match('/^\d{2}(0[1-9]|1[0-2])$/', $value);
+        return $key === 'card_expiration_date';
     }
 
     /**
      * @param string $key
-     * @param $value
      * @return bool
      */
-    private static function isCardPassword(string $key, $value): bool
+    private static function isCardPassword(string $key): bool
     {
-        return $key === 'card_password' && is_string($value) && preg_match('/^\d{2}$/', $value);
+        return $key === 'card_password';
     }
 
     /**
      * @param string $key
-     * @param $value
      * @return bool
      */
-    private static function isBirthDate(string $key, $value): bool
+    private static function isTaxId(string $key): bool
     {
-        return $key === 'tax_id' && is_string($value) && preg_match('/^\d{2}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$/', $value);
+        return $key === 'tax_id';
     }
 
     /**
      * @param string $key
-     * @param $value
      * @return bool
      */
-    private static function isPin(string $key, $value): bool
+    private static function isPin(string $key): bool
     {
-        return $key === 'pin' && is_string($value) && preg_match('/\d{6}/', $value);
+        return $key === 'pin';
     }
 
     /**
      * @param string $key
-     * @param $value
      * @return bool
      */
-    private static function isBuyerName(string $key, $value): bool
+    private static function isBuyerName(string $key): bool
     {
-        return $key === 'buyer_name' && is_string($value) && preg_match('/.+/', $value);
+        return $key === 'buyer_name';
     }
 
     /**
      * @param string $key
-     * @param $value
      * @return bool
      */
-    private static function isBuyerEmail(string $key, $value): bool
+    private static function isBuyerEmail(string $key): bool
     {
-        return $key === 'buyer_email' && is_string($value) && preg_match('/.+\@.+\..+/', $value);
+        return $key === 'buyer_email';
     }
 }
