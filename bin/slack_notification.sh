238230a9afb22b317f7d47dd150381ec438a3b8b
Gyu Kang <me@amolorsy.me>
2019-01-02 16:16:09 +0900

    Notify via slack after deploy

---
+++ bin/slack_notification.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ "$1" = "success" ]
+then
+  SLACK_MESSAGE_TITLE="Success"
+  SLACK_MESSAGE_COLOR="#2ecc71"
+elif [ "$1" = "fail" ]
+then
+  SLACK_MESSAGE_TITLE="Fail"
+  SLACK_MESSAGE_COLOR="#f35a00"
+fi
+
+curl -X POST \
+  -H 'Content-type: application/json' \
+  --data '{
+    "attachments": [{
+      "title": "'"${SLACK_MESSAGE_TITLE}"'",
+      "text": "'"${TRAVIS_COMMIT_MESSAGE}"'",
+      "color": "'"${SLACK_MESSAGE_COLOR}"'",
+      "fields": [
+        { "title": "Revision", "value": "'"${TRAVIS_COMMIT}"'" }
+      ]
+    }]
+  }' \
+  ${SLACK_WEBHOOK_URL}
