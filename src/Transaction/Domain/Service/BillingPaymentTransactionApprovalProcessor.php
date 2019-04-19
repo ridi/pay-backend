bae80b45b70d91e1a0c3ec8f64f8f749684a22a7
Gyu Kang <me@amolorsy.me>
2019-04-19 19:40:22 +0900

    Move common logics for transaction approval to trait

--- src/Transaction/Domain/Service/BillingPaymentTransactionApprovalProcessor.php
+++ src/Transaction/Domain/Service/BillingPaymentTransactionApprovalProcessor.php
@@ -5,9 +5,7 @@ namespace RidiPay\Transaction\Domain\Service;
 
 use Ramsey\Uuid\Uuid;
 use RidiPay\Kernel;
-use RidiPay\Library\EntityManagerProvider;
 use RidiPay\Library\IdempotentRequestProcessor;
-use RidiPay\Library\SentryHelper;
 use RidiPay\Pg\Application\Dto\PgDto;
 use RidiPay\Pg\Application\Service\PgAppService;
 use RidiPay\Pg\Domain\Exception\TransactionApprovalException;
@@ -15,13 +13,10 @@ use RidiPay\Pg\Domain\Exception\UnsupportedPgException;
 use RidiPay\Pg\Domain\Service\Buyer;
 use RidiPay\Pg\Domain\Service\PgHandlerFactory;
 use RidiPay\Pg\Domain\Service\PgHandlerInterface;
-use RidiPay\Pg\Domain\Service\TransactionApprovalResponse;
 use RidiPay\Transaction\Domain\Entity\SubscriptionEntity;
 use RidiPay\Transaction\Domain\Entity\TransactionEntity;
-use RidiPay\Transaction\Domain\Entity\TransactionHistoryEntity;
 use RidiPay\Transaction\Domain\Exception\NotFoundSubscriptionException;
 use RidiPay\Transaction\Domain\Repository\SubscriptionRepository;
-use RidiPay\Transaction\Domain\Repository\TransactionHistoryRepository;
 use RidiPay\Transaction\Domain\Repository\TransactionRepository;
 use RidiPay\User\Application\Service\PaymentMethodAppService;
 use RidiPay\User\Domain\Exception\DeletedPaymentMethodException;
@@ -29,6 +24,8 @@ use RidiPay\User\Domain\Exception\UnregisteredPaymentMethodException;
 
 class BillingPaymentTransactionApprovalProcessor extends IdempotentRequestProcessor
 {
+    use TransactionApprovalTrait;
+
     /** @var SubscriptionEntity */
     private $subscription;
 
@@ -95,7 +92,6 @@ class BillingPaymentTransactionApprovalProcessor extends IdempotentRequestProces
      * @throws DeletedPaymentMethodException
      * @throws TransactionApprovalException
      * @throws UnregisteredPaymentMethodException
-     * @throws UnsupportedPgException
      * @throws \Doctrine\DBAL\DBALException
      * @throws \Doctrine\ORM\ORMException
      * @throws \Throwable
@@ -103,16 +99,8 @@ class BillingPaymentTransactionApprovalProcessor extends IdempotentRequestProces
     protected function run(): BillingPaymentTransactionApprovalResult
     {
         $transaction = $this->createTransaction();
-
         $pg_bill_key = PaymentMethodAppService::getBillingPaymentPgBillKey($transaction->getPaymentMethodId());
-        $pg_response = $this->pg_handler->approveTransaction($transaction, $pg_bill_key, $this->buyer);
-        if (!$pg_response->isSuccess()) {
-            $this->createTransactionHistory($transaction, $pg_response);
-
-            throw new TransactionApprovalException($pg_response->getResponseMessage());
-        }
-
-        $transaction = $this->makeTransactionApproved($transaction, $pg_response);
+        $transaction = self::approveTransaction($transaction, $this->pg_handler, $pg_bill_key, $this->buyer);
 
         return new BillingPaymentTransactionApprovalResult(
             $this->subscription->getUuid()->toString(),
@@ -192,95 +180,5 @@ class BillingPaymentTransactionApprovalProcessor extends IdempotentRequestProces
         TransactionRepository::getRepository()->save($transaction);
 
         return $transaction;
-    }
-
-    /**
-     * @param TransactionEntity $transaction
-     * @param TransactionApprovalResponse $pg_response
-     * @return TransactionHistoryEntity
-     * @throws \Doctrine\DBAL\DBALException
-     * @throws \Doctrine\ORM\ORMException
-     */
-    private function createTransactionHistory(
-        TransactionEntity $transaction,
-        TransactionApprovalResponse $pg_response
-    ): TransactionHistoryEntity {
-        $transaction_history = TransactionHistoryEntity::createApproveHistory(
-            $transaction,
-            $pg_response->isSuccess(),
-            $pg_response->getResponseCode(),
-            $pg_response->getResponseMessage()
-        );
-        TransactionHistoryRepository::getRepository()->save($transaction_history);
-
-        return $transaction_history;
-    }
-
-    /**
-     * @param TransactionEntity $transaction
-     * @param TransactionApprovalResponse $pg_response
-     * @return TransactionEntity
-     * @throws TransactionApprovalException
-     * @throws UnsupportedPgException
-     * @throws \Doctrine\DBAL\DBALException
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Throwable
-     */
-    private function makeTransactionApproved(
-        TransactionEntity $transaction,
-        TransactionApprovalResponse $pg_response
-    ): TransactionEntity {
-        $em = EntityManagerProvider::getEntityManager();
-        $em->beginTransaction();
-
-        try {
-            $transaction->approve($pg_response->getPgTransactionId());
-            TransactionRepository::getRepository()->save($transaction);
-
-            $this->createTransactionHistory($transaction, $pg_response);
-
-            $em->commit();
-        } catch (\Throwable $t) {
-            $em->rollback();
-            $em->close();
-
-            $this->refundPgTransaction($transaction);
-
-            throw $t;
-        }
-
-        return $transaction;
-    }
-
-    /**
-     * 결제 승인 건 환불 처리
-     *
-     * @param TransactionEntity $transaction
-     * @throws TransactionApprovalException
-     */
-    private function refundPgTransaction(TransactionEntity $transaction): void
-    {
-        $cancel_reason = '[정기 결제] PG사 결제 성공 후, 내부 승인 처리 중 오류 발생';
-        $cancel_transaction_response = $this->pg_handler->cancelTransaction(
-            $transaction->getPgTransactionId(),
-            $cancel_reason
-        );
-        if (!$cancel_transaction_response->isSuccess()) {
-            $message = '[정기 결제] PG사 결제 성공 후, 내부 승인 처리 중 오류 발생으로 인한 PG사 결제 취소 중 오류 발생';
-
-            $data = [
-                'extra' => [
-                    'partner_transaction_id' => $transaction->getPartnerTransactionId(),
-                    'transaction_id' => $transaction->getId(),
-                    'pg_transaction_id' => $transaction->getPgTransactionId(),
-                    'subscription_id' => $this->subscription->getId(),
-                    'pg_response_code' => $cancel_transaction_response->getResponseCode(),
-                    'pg_response_message' => $cancel_transaction_response->getResponseMessage()
-                ]
-            ];
-            SentryHelper::captureMessage($message, [], $data, true);
-
-            throw new TransactionApprovalException($cancel_transaction_response->getResponseMessage());
-        }
     }
 }
--- src/Transaction/Domain/Service/OneTimePaymentTransactionApprovalProcessor.php
+++ src/Transaction/Domain/Service/OneTimePaymentTransactionApprovalProcessor.php
@@ -5,9 +5,7 @@ namespace RidiPay\Transaction\Domain\Service;
 
 use Ramsey\Uuid\Uuid;
 use RidiPay\Kernel;
-use RidiPay\Library\EntityManagerProvider;
 use RidiPay\Library\IdempotentRequestProcessor;
-use RidiPay\Library\SentryHelper;
 use RidiPay\Pg\Application\Dto\PgDto;
 use RidiPay\Pg\Application\Service\PgAppService;
 use RidiPay\Pg\Domain\Exception\TransactionApprovalException;
@@ -15,12 +13,9 @@ use RidiPay\Pg\Domain\Exception\UnsupportedPgException;
 use RidiPay\Pg\Domain\Service\Buyer;
 use RidiPay\Pg\Domain\Service\PgHandlerFactory;
 use RidiPay\Pg\Domain\Service\PgHandlerInterface;
-use RidiPay\Pg\Domain\Service\TransactionApprovalResponse;
 use RidiPay\Transaction\Domain\Entity\TransactionEntity;
-use RidiPay\Transaction\Domain\Entity\TransactionHistoryEntity;
 use RidiPay\Transaction\Domain\Exception\AlreadyCancelledTransactionException;
 use RidiPay\Transaction\Domain\Exception\NotFoundTransactionException;
-use RidiPay\Transaction\Domain\Repository\TransactionHistoryRepository;
 use RidiPay\Transaction\Domain\Repository\TransactionRepository;
 use RidiPay\User\Application\Service\PaymentMethodAppService;
 use RidiPay\User\Domain\Exception\DeletedPaymentMethodException;
@@ -28,6 +23,8 @@ use RidiPay\User\Domain\Exception\UnregisteredPaymentMethodException;
 
 class OneTimePaymentTransactionApprovalProcessor extends IdempotentRequestProcessor
 {
+    use TransactionApprovalTrait;
+
     /** @var TransactionEntity */
     private $transaction;
 
@@ -73,22 +70,15 @@ class OneTimePaymentTransactionApprovalProcessor extends IdempotentRequestProces
     protected function run(): OneTimePaymentTransactionApprovalResult
     {
         $pg_bill_key = PaymentMethodAppService::getOneTimePaymentPgBillKey($this->transaction->getPaymentMethodId());
-        $pg_response = $this->pg_handler->approveTransaction($this->transaction, $pg_bill_key, $this->buyer);
-        if (!$pg_response->isSuccess()) {
-            $this->createTransactionHistory($pg_response);
-
-            throw new TransactionApprovalException($pg_response->getResponseMessage());
-        }
-        
-        $this->makeTransactionApproved($pg_response);
+        $transaction = self::approveTransaction($this->transaction, $this->pg_handler, $pg_bill_key, $this->buyer);
 
         return new OneTimePaymentTransactionApprovalResult(
-            $this->transaction->getUuid()->toString(),
-            $this->transaction->getPartnerTransactionId(),
-            $this->transaction->getProductName(),
-            $this->transaction->getAmount(),
-            $this->transaction->getReservedAt(),
-            $this->transaction->getApprovedAt()
+            $transaction->getUuid()->toString(),
+            $transaction->getPartnerTransactionId(),
+            $transaction->getProductName(),
+            $transaction->getAmount(),
+            $transaction->getReservedAt(),
+            $transaction->getApprovedAt()
         );
     }
 
@@ -136,86 +126,6 @@ class OneTimePaymentTransactionApprovalProcessor extends IdempotentRequestProces
     {
         if ($transaction->isCanceled()) {
             throw new AlreadyCancelledTransactionException();
-        }
-    }
-
-    /**
-     * @param TransactionApprovalResponse $pg_response
-     * @return TransactionHistoryEntity
-     * @throws \Doctrine\DBAL\DBALException
-     * @throws \Doctrine\ORM\ORMException
-     */
-    private function createTransactionHistory(TransactionApprovalResponse $pg_response): TransactionHistoryEntity
-    {
-        $transaction_history = TransactionHistoryEntity::createApproveHistory(
-            $this->transaction,
-            $pg_response->isSuccess(),
-            $pg_response->getResponseCode(),
-            $pg_response->getResponseMessage()
-        );
-        TransactionHistoryRepository::getRepository()->save($transaction_history);
-
-        return $transaction_history;
-    }
-
-    /**
-     * @param TransactionApprovalResponse $pg_response
-     * @throws TransactionApprovalException
-     * @throws \Doctrine\DBAL\DBALException
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Throwable
-     */
-    private function makeTransactionApproved(TransactionApprovalResponse $pg_response): void
-    {
-        $em = EntityManagerProvider::getEntityManager();
-        $em->beginTransaction();
-
-        try {
-            $this->transaction->approve($pg_response->getPgTransactionId());
-            TransactionRepository::getRepository()->save($this->transaction);
-
-            $this->createTransactionHistory($pg_response);
-
-            $em->commit();
-        } catch (\Throwable $t) {
-            $em->rollback();
-            $em->close();
-
-            $this->refundPgTransaction();
-
-            throw $t;
-        }
-    }
-
-    /**
-     * 결제 승인 건 환불 처리
-     *
-     * @throws TransactionApprovalException
-     */
-    private function refundPgTransaction(): void
-    {
-        // 결제 승인 건 취소 처리
-        $cancel_reason = '[단건 결제] PG사 결제 성공 후, 내부 승인 처리 중 오류 발생';
-        $cancel_transaction_response = $this->pg_handler->cancelTransaction(
-            $this->transaction->getPgTransactionId(),
-            $cancel_reason
-        );
-        if (!$cancel_transaction_response->isSuccess()) {
-            $message = '[단건 결제] PG사 결제 성공 후, 내부 승인 처리 중 오류 발생으로 인한 PG사 결제 취소 중 오류 발생';
-
-            $data = [
-                'extra' => [
-                    'partner_transaction_id' => $this->transaction->getPartnerTransactionId(),
-                    'transaction_id' => $this->transaction->getId(),
-                    'pg_transaction_id' => $this->transaction->getPgTransactionId(),
-                    'pg_id' => $this->transaction->getPgId(),
-                    'pg_response_code' => $cancel_transaction_response->getResponseCode(),
-                    'pg_response_message' => $cancel_transaction_response->getResponseMessage()
-                ]
-            ];
-            SentryHelper::captureMessage($message, [], $data, true);
-
-            throw new TransactionApprovalException($cancel_transaction_response->getResponseMessage());
         }
     }
 }
