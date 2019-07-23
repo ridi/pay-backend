c23854af317cc48688363ed8ebfcecbdd18bc2b6
Gyu Kang <me@amolorsy.me>
2019-07-23 17:20:13 +0900

    Simplify log sanitization

--- src/Controller/Logger/ControllerAccessLogSanitizationProcessor.php
+++ src/Controller/Logger/ControllerAccessLogSanitizationProcessor.php
@@ -15,13 +15,16 @@ class ControllerAccessLogSanitizationProcessor implements ProcessorInterface
     {
         $message_components = json_decode($records['message'], true);
         if (isset($message_components['request_body'])) {
-            $sanitized_body = [];
-            foreach (json_decode($message_components['request_body']) as $key => $value) {
-                $sanitized_body[$key] = self::sanitize($key, $value);
-            }
-            $message_components['request_body'] = json_encode($sanitized_body);
+            $decoded_request_body = json_decode($message_components['request_body'], true);
+            if (is_array($decoded_request_body) && !empty($decoded_request_body)) {
+                $sanitized_body = [];
+                foreach ($decoded_request_body as $key => $value) {
+                    $sanitized_body[$key] = self::sanitize($key, $value);
+                }
+                $message_components['request_body'] = json_encode($sanitized_body);
 
-            $records['message'] = json_encode($message_components);
+                $records['message'] = json_encode($message_components);
+            }
         }
 
         return $records;
@@ -34,85 +37,23 @@ class ControllerAccessLogSanitizationProcessor implements ProcessorInterface
      */
     private static function sanitize(string $key, $value)
     {
-        if (self::isCardNumber($key)) {
-            $value = substr($value, 0, 6) . str_repeat('*', strlen($value) - 6);
-        } elseif (self::isCardExpirationDate($key)) {
-            $value = '****';
-        } elseif (self::isCardPassword($key)) {
-            $value = '**';
-        } elseif (self::isTaxId($key)) {
-            $value = str_repeat('*', strlen($value));
-        } elseif (self::isPin($key)) {
-            $value = '******';
-        } elseif (self::isBuyerName($key)) {
-            $value = str_repeat('*', mb_strlen($value));
-        } elseif (self::isBuyerEmail($key)) {
-            $value = str_repeat('*', mb_strlen($value));
+        if ($key === 'card_number' && preg_match('/^\d{13,16}$/', strval($value))) {
+            return substr(strval($value), 0, 6) . '*';
+        } elseif (in_array(
+            $key,
+            [
+                'card_expiration_date',
+                'card_password',
+                'tax_id',
+                'pin',
+                'buyer_name',
+                'buyer_email'
+            ],
+            true
+        )) {
+            return '*';
+        } else {
+            return $value;
         }
-
-        return $value;
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    private static function isCardNumber(string $key): bool
-    {
-        return $key === 'card_number';
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    private static function isCardExpirationDate($key): bool
-    {
-        return $key === 'card_expiration_date';
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    private static function isCardPassword(string $key): bool
-    {
-        return $key === 'card_password';
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    private static function isTaxId(string $key): bool
-    {
-        return $key === 'tax_id';
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    private static function isPin(string $key): bool
-    {
-        return $key === 'pin';
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    private static function isBuyerName(string $key): bool
-    {
-        return $key === 'buyer_name';
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    private static function isBuyerEmail(string $key): bool
-    {
-        return $key === 'buyer_email';
     }
 }
